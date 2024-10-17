d92c134ab054b399aa5c8a7af69a3773d6ddc286
Tomás García Chávez <work.garciatomaschavez@gmail.com>
2024-10-17 12:20:21 +0200

    Added theming to the buttons, added a signup view and small fucntionality for the signup and cancel signup buttons

--- app/src/main/java/com/garciatomas/nicestart/SignUp.java
+++ app/src/main/java/com/garciatomas/nicestart/SignUp.java
@@ -10,13 +10,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Login extends AppCompatActivity {
+public class SignUp extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_sign_up);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -24,8 +24,8 @@ public class Login extends AppCompatActivity {
         });
     }
 
-    public void openSignup(View view) {
-        Intent intent = new Intent(Login.this, SignUp.class);
+    public void openLogin(View view) {
+        Intent intent = new Intent(SignUp.this, Login.class);
         startActivity(intent);
     }
 }
