bdefa03e5df3581c84b8fd958e2bc04aa8945335
Tomás García Chávez <work.garciatomaschavez@gmail.com>
2024-10-23 14:04:23 +0200

    Worked a bit on glide (not working yet)

--- app/src/main/java/com/garciatomas/nicestart/Login.java
+++ app/src/main/java/com/garciatomas/nicestart/Login.java
@@ -1,8 +1,10 @@
 package com.garciatomas.nicestart;
 
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,10 +12,27 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+
 public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        // Glide for loading person
+        ImageView mPerson = findViewById(R.id.person);
+
+        Glide.with(this)
+//                .load(https://images.unsplash.com/photo-1527164561913-76d911aa17ff?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)
+                .load(R.drawable.person)
+//                .transition(DrawableTransitionOptions.withCrossFade(2000))
+//                .centerCrop()
+//                .circleCrop()
+//                .placeholder(new ColorDrawable(this.getResources().getColor(R.color.Brunswick_green)))
+                .into(mPerson);
+
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
