5d2ab6fc09a90149f0497ca1ecd982cb847e5ad2
Min Wang <minwang807@gmail.com>
2024-11-27 13:59:59 +0100

    .

--- app/src/main/java/com/garciatomas/nicestart/Profile.java
+++ app/src/main/java/com/garciatomas/nicestart/Profile.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,7 +16,7 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 
 public class Profile extends AppCompatActivity {
-
+private TextView textView1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
